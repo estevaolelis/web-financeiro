b3ab4422bd3f6c97edd67344754b71e5020a49f1
estevaolelis <estevaolelis@gmail.com>
2025-12-10 16:23:21 -0300

    fix: tela de login e ajuste de cor

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { LoginForm } from "@/components/login-form";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
+      <div className="w-full max-w-sm">
+        <LoginForm className="w-full"/>
+      </div>
     </div>
   );
 }
